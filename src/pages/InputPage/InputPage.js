153f336cd69ee77af5feed8f1863492f4e7e2f60
Dario Djuric <dario.djuric@altima.hr>
2021-04-19 18:59:32 +0200

    Implement input page

--- src/pages/InputPage/InputPage.js
+++ src/pages/InputPage/InputPage.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React from "react";
+import { Container, Form } from "semantic-ui-react";
+import { useHistory } from "react-router-dom";
 
 export function InputPage() {
+  const [username, setUsername] = React.useState("");
+  const history = useHistory();
+
+  const handleChange = (e, { value }) => {
+    setUsername(value);
+  };
+
+  const handleSubmit = () => {
+    history.push(`/${encodeURIComponent(username)}`);
+  };
+
   return (
-    <>
-      Input form
-    </>
+    <Container textAlign="center">
+      <Form onSubmit={handleSubmit}>
+        <Form.Input
+          style={{ width: "40%", paddingTop: "10%" }}
+          placeholder="Search users..."
+          onChange={handleChange}
+          value={username}
+        />
+
+        <Form.Button>Search</Form.Button>
+      </Form>
+    </Container>
   );
 }
