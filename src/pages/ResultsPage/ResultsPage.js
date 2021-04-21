b03498183deaa1382877f41cef0c66aa614b8173
Dario Djuric <dario.djuric@altima.hr>
2021-04-21 17:35:52 +0200

    Remove redundant import

--- src/pages/ResultsPage/ResultsPage.js
+++ src/pages/ResultsPage/ResultsPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import GitHubApi from '../../api';
-import { Button, Container, Message } from 'semantic-ui-react';
+import { Container, Message } from 'semantic-ui-react';
 import { Item } from './Item';
 
 export function ResultsPage() {
